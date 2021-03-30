1cb647570c7f47617b50e29313651d7d5c1a710d
Silas Albrecht <silas.a.a@gmail.com>
2021-03-30 08:51:33 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-header">
+      <img src={logo} alt="logo" />
+      <select name="select-bg" id="select-bg"></select>
+    </div>
+  );
+}
+
+export default App;
